c45fa2b97a69f58e6afc5ee7599989946545d34b
yessetz <yessetkhan@gmail.com>
2022-07-29 01:34:17 +0500

    feat: add morse-decode functionality

--- src/index.js
+++ src/index.js
@@ -38,7 +38,60 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let map = { '10' : '.', '11' : '-', "**" : ' ' };
+    let morse = [];
+
+    for (let w = 0; w < expr.length; w += 10) {
+
+        let wArr = [];
+
+        for (let l = w; l < w + 10; l += 2) {
+
+            let letter = expr[l] + expr[l+1];
+
+            for (let m in map) {
+
+                // console.log("Map: " + m + " is " + map[m] + "  Letter: " + letter )
+                
+                if (m === letter) { 
+
+                    wArr.push(map[m]);
+                    break;
+
+                }
+
+            }
+            
+        }
+
+        if (wArr[0] === ' ')
+            wArr = [' '];
+
+        morse.push(wArr.join(''));
+    }
+
+    let word = new Array();
+
+    for (let i in morse) {
+        
+        if (morse[i] === ' ') {
+            word += morse[i];
+        }
+        else {
+
+            for (let j in MORSE_TABLE) {
+
+                if (morse[i] === j) {
+                    word += MORSE_TABLE[j];
+                    break;
+                }
+                
+            }
+
+        }
+    }
+
+    return word;
 }
 
 module.exports = {
